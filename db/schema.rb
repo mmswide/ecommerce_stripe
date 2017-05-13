b1999305cca07edb57857630346506c8469f1324
mobileguru121 <mobileguru121@gmail.com>
2017-05-12 20:15:16 -0400

    update views to pull information from the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512125111) do
+ActiveRecord::Schema.define(version: 20170512132054) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170512125111) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "info"
   end
 
   create_table "purchases", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 niceCat = Product.create(
   title: "Quality Kitty",
@@ -13,6 +13,7 @@ niceCat = Product.create(
   image_name: "kitty.JPG",
   price: "4.99",
   sku: "kittyOne",
+  info: "cute cat",
   download_url: "https://s3-us-west-2.amazonaws.com/ecommerce-stripe/kitty.JPG",
   details: "A really nice cat",
   description: %{<p>It's a cat</p>})
@@ -23,6 +24,7 @@ niceDog = Product.create(
   image_name: "dog.JPG",
   price: "4.99",
   sku: "dogOne",
+  info: "cute pup",
   download_url: "https://s3-us-west-2.amazonaws.com/ecommerce-stripe/dog.JPG",
   details: "A really nice dog",
   description: %{<p>It's a dog</p>})
